0e5bf2032cecb67afdfe3288edf5d2880b372c2d
Iskier2 <iskrafilip12@interia.pl>
2024-05-16 19:48:36 +0200

    matlab-8

--- include/matlab.hpp
+++ include/matlab.hpp
@@ -5,11 +5,16 @@
 #include <cstddef>
 #include <vector>
 #include <cmath>
-#include <vector>
 #include <string>
 #include <numeric>
+#include "matlab.hpp"
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <algorithm>
 
 namespace Matlab {
+    template<typename T>
     class Vector {
     private:
         std::vector<int> v_;
@@ -44,11 +49,12 @@ namespace Matlab {
         int &operator[](std::size_t pos) { return v_[pos]; }  // mutator
     };
 
+    template<typename T>
     class Matrix {
     private:
-        std::vector<Vector> matrix_;
+        std::vector<Vector<T>> matrix_;
     public:
-        Matrix(std::size_t n_rows, std::size_t n_cols) : matrix_(n_rows, Vector(n_cols)) {};
+        Matrix(std::size_t n_rows, std::size_t n_cols) : matrix_(n_rows, Vector<T>(n_cols)) {};
 
         Matrix(const Matrix &) = default;
 
@@ -58,34 +64,106 @@ namespace Matlab {
 
         [[nodiscard]] double sum() const;
 
-        Vector &operator[](const size_t &index) { return matrix_[index]; };
+        Vector<T> &operator[](const size_t &index) { return matrix_[index]; };
 
-        const Vector &operator[](const size_t &index) const { return matrix_[index]; };
+        const Vector<T> &operator[](const size_t &index) const { return matrix_[index]; };
 
-        std::vector<Vector>::iterator begin() { return matrix_.begin(); };
+        typename std::vector<Vector<T>>::iterator begin() { return matrix_.begin(); };
 
-        std::vector<Vector>::iterator end() { return matrix_.end(); };
+        typename std::vector<Vector<T>>::iterator end() { return matrix_.end(); };
 
         [[nodiscard]] std::size_t size() const { return matrix_.size(); };
 
-        [[nodiscard]] std::vector<Vector>::const_iterator begin() const { return matrix_.cbegin(); };
+        [[nodiscard]] typename std::vector<Vector<T>>::const_iterator begin() const { return matrix_.cbegin(); };
 
-        [[nodiscard]] std::vector<Vector>::const_iterator end() const { return matrix_.cend(); };
+        [[nodiscard]] typename std::vector<Vector<T>>::const_iterator end() const { return matrix_.cend(); };
 
-        [[nodiscard]] std::vector<Vector>::const_iterator cbegin() const { return matrix_.cbegin(); };
+        [[nodiscard]] typename std::vector<Vector<T>>::const_iterator cbegin() const { return matrix_.cbegin(); };
 
-        [[nodiscard]] std::vector<Vector>::const_iterator cend() const { return matrix_.cend(); };
+        [[nodiscard]] typename std::vector<Vector<T>>::const_iterator cend() const { return matrix_.cend(); };
 
     };
 
-    Vector add_vectors(const Vector &v1, const Vector &v2);
+    template<typename T>
+    Vector<T> add_vectors(const Vector<T> &v1, const Vector<T> &v2);
 
-    Matrix add_matrices(const Matrix &m1, const Matrix &m2);
+    template<typename T>
+    Matrix<T> add_matrices(const Matrix<T> &m1, const Matrix<T> &m2);
 
-    std::string to_string(const Vector &vec);
+    template<typename T>
+    std::string to_string(const Vector<T> &vec);
 
-    std::string to_string(const Matrix &m);
+    template<typename T>
+    std::string to_string(const Matrix<T> &m);
 
-    std::string was_exception_raised_when_adding_vectors(const Vector& v1, const Vector& v2);
+    template<typename T>
+    std::string was_exception_raised_when_adding_vectors(const Vector<T>& v1, const Vector<T>& v2);
+}
+
+///////////////////////////////////////
+
+
+template<typename T>
+Matlab::Vector<T> Matlab::add_vectors(const Matlab::Vector<T>&v1, const Matlab::Vector<T>&v2) {
+    if(v1.size() != v2.size())
+        throw std::invalid_argument("Vectors have unequal size (" + std::to_string(v1.size()) + " and " + std::to_string(v2.size()) + ").");
+    Matlab::Vector<T> s(v1.size());
+    std::transform(v1.cbegin(), v1.cend(), v2.cbegin(), s.begin(), std::plus<>());
+    return s;
+}
+
+template<typename T>
+Matlab::Matrix<T> Matlab::add_matrices(const Matlab::Matrix<T>&m1, const Matlab::Matrix<T>&m2) {
+    Matlab::Matrix resp(m1);
+    std::transform(m1.begin(), m1.end(), m2.begin(), resp.begin(), add_vectors<T>);
+    return resp;
+}
+
+template<typename T>
+void Matlab::Vector<T>::print_vector() const{
+    for (const int& i : v_)std::cout<<i<<", ";
+    std::cout<<std::endl;
+}
+
+template<typename T>
+double Matlab::Vector<T>::norm() const {
+    return std::sqrt(std::accumulate(begin(), end(), 0, [](double acc, const double& val){return acc + val * val;}));
+}
+
+template<typename T>
+std::string Matlab::to_string(const Matlab::Vector<T>&vec){
+    std::ostringstream stream;
+    stream << "[ ";
+    for(auto i = vec.cbegin(); i != vec.cend(); i++)
+        stream << *i << (i == vec.cend() - 1 ? "" : ", ");
+    stream << " ]";
+    return stream.str();
+}
+
+template<typename T>
+std::string Matlab::to_string(const Matlab::Matrix<T>&m){
+    std::ostringstream stream;
+    stream << "[" << std::endl;
+    for(const Matlab::Vector<T>&i : m) stream << "\t" << Matlab::to_string(i) << std::endl;
+    stream << "]" << std::endl;
+    return stream.str();
+}
+
+template<typename T>
+double Matlab::Matrix<T>::sum() const {
+    return std::accumulate(matrix_.begin(), matrix_.end(), 0,
+                           [](int accumulator, const Matlab::Vector<T>&vector) {
+                               return accumulator + vector.sum();
+                           });
+}
+
+template<typename T>
+std::string Matlab::was_exception_raised_when_adding_vectors(const Matlab::Vector<T>&v1, const Matlab::Vector<T>&v2){
+    try {
+        Matlab::add_vectors(v1, v2);
+        return"";
+    }catch (const std::invalid_argument& err){
+        return err.what();
+    }
 }
 #endif /* INCLUDE_MATLAB_HPP_ */
--- test/test_vector.cpp
+++ test/test_vector.cpp
@@ -3,45 +3,45 @@
 #include "matlab.hpp"
 
 TEST(MatlabVectorTest, norm) {
-    auto testVect1 = Matlab::Vector(3);
+    auto testVect1 = Matlab::Vector<int>(3);
     testVect1[0] = 3;
     testVect1[1] = 4;
     testVect1[2] = 0;
     EXPECT_EQ(testVect1.norm(), 5);
 }
 TEST(MatlabVectorTest, createWithSize) {
-    Matlab::Vector v(2U);
+    Matlab::Vector<int>v(2U);
 
     ASSERT_EQ(v.size(), 2U);  // przyrostek "U" oznacza wartość bez znaku (ang. unsigned)
     EXPECT_EQ(v[0], 0);
     EXPECT_EQ(v[1], 0);
 }
 TEST(MatlabVectorTest, add) {
-    Matlab::Vector v1(4U);
-    Matlab::Vector v2(4U);
+    Matlab::Vector<int>v1(4U);
+    Matlab::Vector<int>v2(4U);
     v1[0] = 5;
     v1[1] = 2;
     v1[2] = 7;
     v2[0] = 2;
     v2[1] = 5;
-    Matlab::Vector v3 = add_vectors(v1, v2);
+    Matlab::Vector<int>v3 = add_vectors(v1, v2);
     EXPECT_EQ(v3[0], 7);
     EXPECT_EQ(v3[1], 7);
     EXPECT_EQ(v3[2], 7);
     EXPECT_EQ(v3[3], 0);
 }
 TEST(MatlabVectorTest, toSting) {
-    Matlab::Vector v({1, 2, 3});
+    Matlab::Vector<int>v({1, 2, 3});
     EXPECT_EQ(to_string(v), "[ 1, 2, 3 ]");
 }
 
 TEST(MatlabVectorTest, dodatkowe){
-    Matlab::Matrix mat(5, 5);
+    Matlab::Matrix<int>mat(5, 5);
     mat[1][1] = 1;
     std::cout<<to_string(mat);
-    Matlab::Matrix mat2({{1, 2}, {3, 4}});
+    Matlab::Matrix<int>mat2({{1, 2}, {3, 4}});
     std::cout<<to_string(mat2);
-    Matlab::Matrix mat3({{9, 8}, {7, 6}});
+    Matlab::Matrix<int>mat3({{9, 8}, {7, 6}});
     std::cout<<to_string(add_matrices(mat2, mat3));
     std::cout<<mat3.sum()<<" "<<mat3[1][1];
 }
@@ -49,18 +49,18 @@ TEST(MatlabVectorTest, addVectorsOfSameSize ){
     std::vector<int> v1({1,2});
     std::vector<int> v2({3,4});
     ASSERT_NO_THROW({
-        Matlab::Vector vs = Matlab::add_vectors(v1, v2);
+        Matlab::Vector<int>vs = Matlab::add_vectors<int>(v1, v2);
         ASSERT_EQ(vs.size(), 2U);
         EXPECT_EQ(vs[0], 4);
         EXPECT_EQ(vs[1], 6);
     });
 }
 TEST(MatlabVectorTest, addVectorsOfDifferentSize ){
-    ASSERT_THROW(Matlab::add_vectors({1},{2}),std::invalid_argument);
+    ASSERT_THROW(Matlab::add_vectors<int>({1},{2}),std::invalid_argument);
 }
 TEST(MatlabVectorTest, exceptionCatchingWhenAddingVectorsOfSameSizes  ){
-    EXPECT_EQ(Matlab::was_exception_raised_when_adding_vectors({3},{3}), "");
+    EXPECT_EQ(Matlab::was_exception_raised_when_adding_vectors<int>({3},{3}), "");
 }
 TEST(MatlabVectorTest, exceptionCatchingWhenAddingVectorsOfDifferentSizes ){
-    EXPECT_EQ(Matlab::was_exception_raised_when_adding_vectors({3},{5}), "Vectors have unequal size (3 and 5).");
+    EXPECT_EQ(Matlab::was_exception_raised_when_adding_vectors<int>({3},{5}), "Vectors have unequal size (3 and 5).");
 }
